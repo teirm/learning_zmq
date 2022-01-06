fb9ca4e8df1e98f3933b40612f85451432f69133
teirm <tribunus.vii@gmail.com>
2022-01-05 20:59:30 -0500

    Found correct version of zhelpers and fixed examples

--- ch_1/wuserver.c
+++ ch_1/wuserver.c
@@ -10,10 +10,7 @@ int main()
 {
     void *context   = zmq_ctx_new();
     void *publisher = zmq_socket(context, ZMQ_PUB);
-    int rc          = zmq_bind(publisher, "tcp://*:5556");
-    assert(rc == 0);
-    
-    rc = zmq_bind(publisher, "ipc://weather.ipc");
+    int rc          = zmq_bind(publisher, "tcp://127.0.0.1:5556");
     assert(rc == 0);
 
     // Initialize random number generator
@@ -21,11 +18,15 @@ int main()
     while (1) {
         // Get mischeaveous values
         int zipcode, temperature, relhumidity;
-        zipcode     = random() % 100000;
-        temperature = (random() % 215) - 80;
-        relhumidity = (random() % 50) + 10;
+        zipcode     = randof(100000); 
+        temperature = randof(215) - 80;
+        relhumidity = randof(50) + 10;
 
         char update[20];
         sprintf(update, "%05d %d %d", zipcode, temperature, relhumidity);
-    }
+
+        s_send(publisher, update);
+    } zmq_close(publisher);
+    zmq_ctx_destroy(context);
+    return 0;
 }
